c31de0242627e05f9a56516f2f94112f63c1cff9
Pourab Karchaudhuri <36023@Hexaware.com>
2019-05-19 22:03:54 +0530

    Added requirements.txt

--- app.py
+++ app.py
@@ -1,8 +1,56 @@
 from flask import Flask, request, jsonify
 import os
 from werkzeug.utils import secure_filename
-import chatbot
+# import chatbot
 
+#!/usr/bin/python3
+import os
+import aiml
+from preprocessor import spellcorrect
+from CHATBOT_LSI import lsa
+
+BRAIN_FILE="brain.dump"
+
+k = aiml.Kernel()
+
+# To increase the startup speed of the bot it is
+# possible to save the parsed aiml files as a
+# dump. This code checks if a dump exists and
+# otherwise loads the aiml from the xml files
+# and saves the brain dump.
+if os.path.exists(BRAIN_FILE):
+    print("Loading from brain file: " + BRAIN_FILE)
+    k.loadBrain(BRAIN_FILE)
+else:
+    print("Parsing aiml files")
+    k.bootstrap(learnFiles="std-startup.aiml", commands="load aiml b")
+    print("Saving brain file: " + BRAIN_FILE)
+    k.saveBrain(BRAIN_FILE)
+
+# Endless loop which passes the input to the bot and prints
+# its response
+# while True:
+#     input_text = input("User says > ")
+
+   
+def get_lsa_response(input_text):
+    response = k.respond(input_text)
+
+    if response=='grammar_fallback' :
+        print("Grammar Engine fallback")
+        spellcorrected_text = spellcorrect(input_text)
+        lsa_response = lsa(spellcorrected_text)
+        # print("Bot says > ", lsa_response)
+        return lsa_response
+
+        # file_path = "/path/to/yourfile.txt"
+        file_path = os.getcwd() + '/fallback_sentences.txt'
+        with open(file_path, 'a') as file:
+            file.write(input_text + "\n")
+
+    else :
+        return response
+        # print("Bot says > ", response)
 
 app = Flask(__name__)
 
@@ -25,7 +73,7 @@ def lsa():
         })
     else:
         print(request.get_json())
-        response = chatbot.get_lsa_response(request.get_json()['query'])
+        response = get_lsa_response(request.get_json()['query'])
         return jsonify({
             "result" : {
                 "fulfillment":{
